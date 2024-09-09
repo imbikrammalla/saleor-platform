ce35278b6db20dd956dcf5788024ad63396af811
bikrammalla58@gmail.com <bikrammalla58@gmail.com>
2024-09-09 16:53:49 +0800

    ISEC6000-assignment1

--- trivy.sh
+++ trivy.sh
@@ -2,6 +2,8 @@ TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # Define the output file name
 OUTPUT_FILE="trivy_files/trivy_results_$TIMESTAMP.txt"
+# Define the output file name
+OUTPUT_FILE="trivy_files/trivy_results_$TIMESTAMP.txt"
 
 # Create the output directory if it doesn't exist
 mkdir -p trivy_files
@@ -11,7 +13,16 @@ IMAGES=("ghcr.io/saleor/saleor:latest" "ghcr.io/saleor/saleor-dashboard:latest" 
 
 # Loop through each image and scan it using Trivy, saving output to the file
 for IMAGE in "${IMAGES[@]}"; do
+# Create the output directory if it doesn't exist
+mkdir -p trivy_files
+
+# Define the list of images to scan
+IMAGES=("ghcr.io/saleor/saleor:latest" "ghcr.io/saleor/saleor-dashboard:latest" "jaegertracing/all-in-one" "postgres:15-alpine" "redis:7.0-alpine")
+
+# Loop through each image and scan it using Trivy, saving output to the file
+for IMAGE in "${IMAGES[@]}"; do
     echo "Scanning $IMAGE..." | tee -a $OUTPUT_FILE
+    trivy image "$IMAGE" | tee -a $OUTPUT_FILE
     trivy image "$IMAGE" | tee -a $OUTPUT_FILE
     echo "-------------------------------------" | tee -a $OUTPUT_FILE
 done
